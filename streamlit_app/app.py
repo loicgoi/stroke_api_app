9344100d94cb62a4d773c0baf2d37d7f8db16b06
[loicgoi] <g.loic.cours@gmail.com>
2025-08-26 12:06:13 +0200

    Mise à jour du .gitignore + Retrait code obsolète en commentaire dans app.py (onglet)

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -3,24 +3,6 @@ from modules import accueil, donnees, visualisations, statistiques
 
 st.set_page_config(page_title="Stroke Prediction App", layout="wide")
 
-# st.title("Stroke Prediction App")
-
-# tab1, tab2, tab3, tab4 = st.tabs(
-#     ["Accueil", "Données", "Visualisation", "Statistiques"]
-# )
-
-# with tab1:
-#     accueil.accueil()
-
-# with tab2:
-#     donnees.donnees()
-
-# with tab3:
-#     visualisations.visualisations()
-
-# with tab4:
-#     statistiques.statistiques()
-
 st.sidebar.title("Menu")
 page = st.sidebar.radio("", ["Accueil", "Données", "Visualisation", "Statistiques"])
 
